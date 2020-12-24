f674203506a34493223bfd3dca7154883724bbf9
archena <archena@users.noreply.github.com>
2020-12-24 09:58:30 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Training
+
+## Setup
+
+```sh
+conda create -n dobble-jetson-nano python=3.8.5
+conda activate dobble-jetson-nano
+```
---
+++ train.py
@@ -0,0 +1,6 @@
+"""
+Dobble is a game with the aim to recognise matching symbols in a pair of disc-shaped cards.
+Dataset and inspiration from https://www.hackster.io/aidventure/the-dobble-challenge-93d57c
+"""
+
+import kaggle
