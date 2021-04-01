9fcc34651a6d32e95bfb4c263bb94729fc8f95c7
D. Lowl <d.lowl@d-lowl.space>
2021-04-01 14:13:56 +0500

    Update README and run.sh script
    Add flags for shell entrypoint, X forwarding, camera mounting and training.
    Update README appropriately.

--- train_object_detection.sh
+++ train_object_detection.sh
@@ -3,7 +3,7 @@
 SSD_BASE_PATH="/jetson-inference/python/training/detection/ssd"
 TRAINING_SCRIPT="$SSD_BASE_PATH/train_ssd.py"
 ONNX_EXPORT="$SSD_BASE_PATH/onnx_export.py"
-PRETRAINED_SSD="$SSD_BASE_PATH/models/mobilenet-v1-ssd-mp-0_675.pth"
+PRETRAINED_SSD="/dobble-jetson-nano/models/ssd/mobilenet-v1-ssd-mp-0_675.pth"
 MODEL_DIR="/dobble-jetson-nano/models/dobble"
 DATA_DIR="/dobble-jetson-nano/data/dobble/voc"
 
